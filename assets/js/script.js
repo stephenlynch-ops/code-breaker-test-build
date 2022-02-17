ff3944d7ab163e60ba262b750aa392d64115b62b
Stephen Lynch <stephen.lynch17@icloud.com>
2022-02-17 18:11:49 +0000

    FIX: Add some JS to try to get the code to run as expected

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     for (let button of buttons){
         button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") !== "start-game") {
-                alert("You need to click Start Game to begin");
+            if (this.getAttribute("data-type") === "submit") {
+                checkAnswer();
             } else {
-                runGame();
+                levelOne();
             }
         })
     }
@@ -18,31 +18,49 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function runGame() {
 
+
     let i = 1;
+    let level = parseInt(document.getElementById("level").innerText);
+    document.getElementById("level").innerText = i.valueOf;
+
+
     while (i < 6) {
 
         if (i = 1) {
-            levelOne();
+            let levelArray = [1, 2, 3, 4, 5]
+            levelOne(levelArray);
+            button.addEventListener("click", function() {
+                if (this.getAttribute("data-type") === "submit") {
+                    checkAnswer(levelArray);
+                }
+            })
         } else {
             console.log("We're not ready for level 2 yet!");
+            i++
         }
     }
 }
 
-function checkAnswer() {
+function checkAnswer(levelArray) {
 
+    console.log(levelArray[3]);
+
+    // let userNumberOne = parseInt(document.getElementById("slot-four").value);
+    // let userNumberTwo = parseInt(document.getElementById("slot-five").value);
+
+    // if (userNumberOne === levelArray[3] && userNumberTwo === levelArray[4]) {
+    //     alert("Correct let's move up a level")
+    // } else {
+    //     alert(`Sorry, but you got it wrong, the answers were ${levelArray[3]} and ${levelArray[4]}.`);
+    // }
 }
 
-function levelOne() {
+function levelOne(levelArray) {
 
-    let levelOneArray = [1, 2, 3, 4, 5];
-
-    let playersClueLevelOneArray = [1, 2, 3, "x", "x"];
-
-    document.getElementById("slot-one").innerText = playersClueLevelOneArray[0];
-    document.getElementById("slot-one").innerText = playersClueLevelOneArray[1];
-    document.getElementById("slot-one").innerText = playersClueLevelOneArray[2];
-    document.getElementById("slot-one").innerText = playersClueLevelOneArray[3];
-    document.getElementById("slot-one").innerText = playersClueLevelOneArray[4];
+    document.getElementById("slot-one").innerText = levelArray[0];
+    document.getElementById("slot-two").innerText = levelArray[1];
+    document.getElementById("slot-three").innerText = levelArray[2];
+    
+    document.getElementById("slot-four").focus;
 
 }
