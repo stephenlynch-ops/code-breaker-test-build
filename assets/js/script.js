0e14d3b2e993d00334c6979c21ef13c4ee20014d
Stephen Lynch <stephen.lynch17@icloud.com>
2022-02-18 16:08:30 +0000

    FEAT: More work on the run game loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
+    //  let button of buttons is the modern version of a for loop with i++ etc.
     for(let button of buttons){
         button.addEventListener("click", function(){
-                alert(`You clicked to submit you answer`)
+            if (this.getAttribute("data-type") === "submit"){
+                collectUsersAnswers();
+            } else {
+                alert(`Game Is Now Stopping`);
+            }
         })
     }
 
+    levelOne();
 })
-
-
 
 function runGame(number) {
 
     let ranNumOne = Math.floor(Math.random() * number) + 1;
     let ranNumTwo = Math.floor(Math.random() * number) + 1;
 
-    console.log(ranNumOne);
-    console.log(ranNumTwo);
+    // console.log(ranNumOne);
+    // console.log(ranNumTwo);
 
     let levelArray = new Array(5);
     
@@ -27,8 +31,13 @@ function runGame(number) {
     levelArray[3] = levelArray[2] + ranNumTwo;
     levelArray[4] = levelArray[3] + ranNumTwo;
 
-    console.log(levelArray);
+    // console.log(levelArray);
 
+    document.getElementById("slot-one").innerText = levelArray[0];
+    document.getElementById("slot-two").innerText = levelArray[1];
+    document.getElementById("slot-three").innerText = levelArray[2];
+
+    
 }
 
 function levelOne() {
@@ -37,3 +46,24 @@ function levelOne() {
     runGame(number);
 
 }
+
+function collectUsersAnswers(passObject) {
+
+    let usersAnswers = new Array(2);
+
+    passObject[0] = parseInt(document.getElementById("slot-four").value);
+    passObject[1] = parseInt(document.getElementById("slot-five").value);
+
+    console.log(usersAnswers[0]);
+    console.log(usersAnswers[1]);
+
+    console.log(passObject);
+
+    if (levelArray[3] === usersAnswers[0] && levelArray[4] === usersAnswers[1]) {
+        alert(`You got it right, lets move on to the next level`);
+    } else {
+        alert(`You got it wrong, the correct answers were ${levelArray[3]} and ${levelArray[4]}. Better luck next time`)
+    }
+
+}
+
