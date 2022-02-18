49a78a5ac3ca0fda3fb46e451cb5de4548913b4f
Stephen Lynch <stephen.lynch17@icloud.com>
2022-02-18 19:51:02 +0000

    FEAT: Add levels 2-4 and completed game loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
+
+document.addEventListener("DOMContentLoaded", function() { 
     let buttons = document.getElementsByTagName("button");
 
     //  let button of buttons is the modern version of a for loop with i++ etc.
@@ -7,10 +8,12 @@ document.addEventListener("DOMContentLoaded", function() {
             if (this.getAttribute("data-type") === "submit"){
                 collectUsersAnswers();
             } else {
-                alert(`Game Is Now Stopping`);
+                alert(`This is here in the event future buttons are added later on i.e. a button to change the difficulty`)
             }
         })
     }
+
+    document.getElementById("slot-four").focus();
 
     levelOne();
 })
@@ -20,8 +23,8 @@ function runGame(number) {
     let ranNumOne = Math.floor(Math.random() * number) + 1;
     let ranNumTwo = Math.floor(Math.random() * number) + 1;
 
-    // console.log(ranNumOne);
-    // console.log(ranNumTwo);
+    console.log("Ran Num 1",ranNumOne);
+    console.log("Ran Num 2", ranNumTwo);
 
     let levelArray = new Array(5);
     
@@ -31,15 +34,24 @@ function runGame(number) {
     levelArray[3] = levelArray[2] + ranNumTwo;
     levelArray[4] = levelArray[3] + ranNumTwo;
 
-    // console.log(levelArray);
+    console.log("Level Array", levelArray);
 
     document.getElementById("slot-one").innerText = levelArray[0];
     document.getElementById("slot-two").innerText = levelArray[1];
     document.getElementById("slot-three").innerText = levelArray[2];
 
-    
+    let missingNums = new Array(2);
+    missingNums[0] = levelArray[3];
+    missingNums[1] = levelArray[4];
+
+    console.log("Missing Nums", missingNums);
+
+    return missingNums;
 }
 
+/**
+ * Level One Number Criteria
+ */
 function levelOne() {
     
     var number = 3;
@@ -47,23 +59,90 @@ function levelOne() {
 
 }
 
-function collectUsersAnswers(passObject) {
-
-    let usersAnswers = new Array(2);
-
-    passObject[0] = parseInt(document.getElementById("slot-four").value);
-    passObject[1] = parseInt(document.getElementById("slot-five").value);
-
-    console.log(usersAnswers[0]);
-    console.log(usersAnswers[1]);
-
-    console.log(passObject);
-
-    if (levelArray[3] === usersAnswers[0] && levelArray[4] === usersAnswers[1]) {
-        alert(`You got it right, lets move on to the next level`);
-    } else {
-        alert(`You got it wrong, the correct answers were ${levelArray[3]} and ${levelArray[4]}. Better luck next time`)
-    }
+/**
+ * Level Two Number Criteria
+ */
+function levelTwo() {
+    
+    var number = 5;
+    runGame(number);
 
 }
 
+/**
+ * Level Three Number Criteria
+ */
+function levelThree() {
+    
+    var number = 7;
+    runGame(number);
+
+}
+
+/**
+ * Level Four Number Criteria
+ */
+function levelFour() {
+    
+    var number = 9;
+    runGame(number);
+
+}
+
+/**
+ * Level Five Number Criteria
+ */
+function levelFive() {
+    
+    var number = 11;
+    runGame(number);
+
+}
+
+function collectUsersAnswers(missingNums) {
+
+        let usersAnswers = new Array(2);
+
+        usersAnswers[0] = parseInt(document.getElementById("slot-four").value);
+        usersAnswers[1] = parseInt(document.getElementById("slot-five").value);
+
+        if ((document.getElementById("slot-four").value) == "" || (document.getElementById("slot-five").value) == "") {
+            throw `User failed to complete the code. Aborting!`;
+        } else {
+            console.log("User Answers", usersAnswers[0]);
+            console.log("User Answers", usersAnswers[1]);
+
+            console.log("Users Ans Array", usersAnswers);
+
+            if (missingNums = usersAnswers) {
+                alert(`You got it right, lets move on to the next level`);
+            } else {
+                alert(`You got it wrong, the correct answers were ${missingNums[0]} and ${missingNums[1]}. Better luck next time`)
+            }
+
+            levelUpdate();
+    }
+}
+
+function levelUpdate() {
+
+    
+
+    let level = 1;
+    let newLevel = level++
+
+    if (newLevel = 2) {
+        levelTwo();
+    } else if (newLevel = 3) {
+        levelThree();
+    } else if (newLevel = 3) {
+        levelFour();
+    } else {
+        levelFive();
+    }
+}
+
+function clearOldAnswers() {
+    document.getElementById("slot-four").value = "";
+    document.getElementById("slot-five").value = "";
+}
