177d88d5c479a661ca6d4168883a074b1ce636ff
Stephen Lynch <stephen.lynch17@icloud.com>
2022-02-18 15:19:48 +0000

    FEAT: Loop for level one to run game now established

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,63 +3,37 @@ document.addEventListener("DOMContentLoaded", function() {
 
     for(let button of buttons){
         button.addEventListener("click", function(){
-            if (this.getAttribute("data-type") === "start-game"){
-                runLevelOne();
-            } else {
-                getUserAnswer();
-            }
+                alert(`You clicked to submit you answer`)
         })
     }
 
-    // Allowing the user to submit the answer from either slot four or five with the enter key
-    document.getElementById("slot-four").addEventListener("keydown", function(event) {
-        if (event.key === "Enter") {
-            getUserAnswer()
-        }
-    })
-
-    document.getElementById("slot-five").addEventListener("keydown", function(event) {
-        if (event.key === "Enter") {
-            getUserAnswer();
-        }
-    })
-
-    runLevelOne();
-
 })
 
-/**
- * Runs the Level one version of the game
- */
-function runLevelOne() {
 
-    document.getElementById("level").innerText = '1';
 
-    let num1 = Math.floor(Math.random() * 3) + 1;
-    let num2 = Math.floor(Math.random() * 3) + 1;
+function runGame(number) {
 
-    let levelOneArray = new Array(5);
+    let ranNumOne = Math.floor(Math.random() * number) + 1;
+    let ranNumTwo = Math.floor(Math.random() * number) + 1;
 
-    levelOneArray[0] = num1;
-    levelOneArray[1] = num1 + num2;
-    levelOneArray[2] = levelOneArray[1] + num2;
-    levelOneArray[3] = levelOneArray[2] + num2;
-    levelOneArray[4] = levelOneArray[3] + num2;
+    console.log(ranNumOne);
+    console.log(ranNumTwo);
 
-    document.getElementById("slot-one").innerText = levelOneArray[0];
-    document.getElementById("slot-two").innerText = levelOneArray[1];
-    document.getElementById("slot-three").innerText = levelOneArray[2];
-
+    let levelArray = new Array(5);
     
+    levelArray[0] = ranNumOne;
+    levelArray[1] = ranNumOne + ranNumTwo;
+    levelArray[2] = levelArray[1] + ranNumTwo;
+    levelArray[3] = levelArray[2] + ranNumTwo;
+    levelArray[4] = levelArray[3] + ranNumTwo;
+
+    console.log(levelArray);
+
 }
 
-/**
- * Collects the users inputs and populates an array with them
- */
-function getUserAnswer() {
-
-    let usersAnswers = new Array(2)
-    usersAnswers[0] = parseInt(document.getElementById("slot-four").value);
-    usersAnswers[1] = parseInt(document.getElementById("slot-five").value);
+function levelOne() {
+    
+    var number = 3;
+    runGame(number);
 
 }
