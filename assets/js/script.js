c1c32a61bf3784214b13c43af07901cf6a2c8a6d
Stephen Lynch <stephen.lynch17@icloud.com>
2022-02-19 13:58:01 +0000

    FEAT: Now a fully working version. Need to finalise end level number

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,181 +12,61 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         })
     }
-
+    document.getElementById("level").innerText = 1;
     document.getElementById("slot-four").focus();
 
-    levelOne();
+    runGame();
 })
 
-function runGame(number) {
+function runGame() {
 
-    let ranNumOne = Math.floor(Math.random() * 10) + 1;
-    let ranNumTwo = number;
+    console.log("---------------------New Game-------------------------");
+    // Get the level number to calculate the required clues   
+    let level = parseInt(document.getElementById("level").innerText);
+    let levelNum = ++level;
 
-    // console.log("Ran Num 1",ranNumOne);
-    console.log("Increasing By: ", ranNumTwo);
+    console.log("Start No:", level);
+    console.log("Adding this each time:", levelNum);
 
-    let levelArray = new Array(5);
-    
-    levelArray[0] = ranNumOne;
-    levelArray[1] = ranNumOne + ranNumTwo;
-    levelArray[2] = levelArray[1] + ranNumTwo;
-    levelArray[3] = levelArray[2] + ranNumTwo;
-    levelArray[4] = levelArray[3] + ranNumTwo;
+    document.getElementById("slot-one").innerText = level;
+    document.getElementById("slot-two").innerText = level + levelNum;
+    document.getElementById("slot-three").innerText = parseInt(document.getElementById("slot-two").innerText) + levelNum;
 
-    // console.log("Level Array", levelArray);
+    let slotFourExpected = level + (levelNum * 3);
+    let slotFiveExpected = slotFourExpected + levelNum;
 
-    document.getElementById("slot-one").innerText = levelArray[0];
-    document.getElementById("slot-two").innerText = levelArray[1];
-    document.getElementById("slot-three").innerText = levelArray[2];
-
-    let missingNums = new Array(2);
-    missingNums[0] = levelArray[3];
-    missingNums[1] = levelArray[4];
-
-    console.log("Missing Nums", missingNums);
-
-    return missingNums;
+    console.log("Slor Four Expected ", slotFourExpected);
+    console.log("Slor Five Expected ", slotFiveExpected);
 }
 
-/**
- * Level One Number Criteria
- */
-function levelOne() {
-    
-    document.getElementById("level").innerText = 1;
+function collectUsersAnswers() {
 
-    var number = 2;
-    runGame(number);
+    // Calculate what the next two numbers should be
 
-}
+    let level = parseInt(document.getElementById("level").innerText);
+    let levelNum = ++level;
 
-/**
- * Level Two Number Criteria
- */
-function levelTwo() {
-    
-    var number = 3;
-    runGame(number);
+    console.log("CUA Start No.:", level);
+    console.log("CUA increase by:",levelNum);
 
-}
+    let slotFourCorrect = level + (levelNum * 3);
+    let slotFiveCorrect = slotFourCorrect + levelNum;
 
-/**
- * Level Three Number Criteria
- */
-function levelThree() {
-    
-    var number = 4;
-    runGame(number);
+    // Collect the users answer for comparison
 
-}
-
-/**
- * Level Four Number Criteria
- */
-function levelFour() {
-    
-    var number = 5;
-    runGame(number);
-
-}
-
-/**
- * Level Five Number Criteria
- */
- function levelFive() {
-    
-    var number = 6;
-    runGame(number);
-
-}
-
-/**
- * Level Six Number Criteria
- */
- function levelSix() {
-    
-    var number = 7;
-    runGame(number);
-
-}
-
-/**
- * Level Seven Number Criteria
- */
- function levelSeven() {
-    
-    var number = 8;
-    runGame(number);
-
-}
-
-/**
- * Level Eight Number Criteria
- */
- function levelEight() {
-    
-    var number = 9;
-    runGame(number);
-
-}
-
-/**
- * Level Nine Number Criteria
- */
- function levelNine() {
-    
-    var number = 10;
-    runGame(number);
-
-}
-
-/**
- * Level Ten Number Criteria
- */
- function levelTen() {
-    
-    var number = 11;
-    runGame(number);
-
-}
-
-/**
- * Level Eleven Number Criteria
- */
- function levelEleven() {
-    
-    var number = 12;
-    runGame(number);
-
-}
-
-
-function collectUsersAnswers(missingNums) {
-
-        let usersAnswers = new Array(2);
-
-        usersAnswers[0] = parseInt(document.getElementById("slot-four").value);
-        usersAnswers[1] = parseInt(document.getElementById("slot-five").value);
+    let slotFourUserAnswer = parseInt(document.getElementById("slot-four").value);
+    let slotFiveUserAnswer = parseInt(document.getElementById("slot-five").value);
 
         if ((document.getElementById("slot-four").value) == "" || (document.getElementById("slot-five").value) == "") {
             throw `User failed to complete the code. Aborting!`;
-        } else {
-            // console.log("User Answers", usersAnswers[0]);
-            // console.log("User Answers", usersAnswers[1]);
-
-            // console.log("Users Ans Array", usersAnswers);
-
-            if (missingNums = usersAnswers) {
-                alert(`You got it right, lets move on to the next level`);
-            } else {
-                alert(`You got it wrong, the correct answers were ${missingNums[0]} and ${missingNums[1]}. Better luck next time`)
-            }
-
+        } else if (slotFourCorrect === slotFourUserAnswer && slotFiveCorrect === slotFiveUserAnswer) {
+            alert(`Thats correct. Well done. Lets move on to level ${levelNum}.`);
             clearOldAnswers();
-
             levelUpdate();
-    }
+        } else {
+            alert(`Sorry thats not correct. The correct asnswers were ${slotFourCorrect} and ${slotFiveCorrect}.`)
+            gameFailed();
+        }    
 }
 
 function levelUpdate() {
@@ -197,45 +77,42 @@ function levelUpdate() {
     document.getElementById("level").innerText = newLevel;
 
     console.log("New Level", newLevel);
-
-    if (newLevel === 2) {
-        levelTwo();
-    } else if (newLevel === 3) {
-        levelThree();
-    } else if (newLevel === 4) {
-        levelFour();
-    } else if (newLevel === 5) {
-        levelFive();
-    } else if (newLevel === 6) {
-        levelSix();
-    } else if (newLevel === 7) {
-        levelSeven();
-    } else if (newLevel === 8) {
-        levelEight();
-    } else if (newLevel === 9) {
-        levelNine();
-    } else if (newLevel === 10) {
-        levelTen();
-    } else if (newLevel === 11) {
-        levelEleven();
-    } else {
-        alert(`You have finished the game. Well done!`);
-    }
+    
+    runGame();
 }
 
+/**
+ * This clears any old answers from the input boxes prior to a new game / level
+ */
 function clearOldAnswers() {
     document.getElementById("slot-four").value = "";
     document.getElementById("slot-five").value = "";
 }
 
+/**
+ * This is the reset function that gives the user the option to reset the game
+ */
 function resetGame() {
     
     let warning = "Confirm RESET game request";
 
     if (confirm(warning) == true) {
-        levelOne();
+        gameFailed();
     } else {
         alert(`Reset request cancelled`);
     }
 
+}
+
+/**
+ * This is the general reset function that is called when the player fails a level
+ * or if the user clicks the reset game button.
+ */
+function gameFailed() {
+    clearOldAnswers();
+
+    document.getElementById("level").innerText = 1;
+    document.getElementById("slot-four").focus();
+
+    runGame();
 }
